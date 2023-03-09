aa49945f5fa05274110333554833c3032d859867
orioljim <orioljay@gmail.com>
2023-03-09 16:16:33 +0100

    Added full head morphing with non-scan models demo

--- scripts/js/morph_target_creator.js
+++ scripts/js/morph_target_creator.js
@@ -1,18 +1,17 @@
 //Script that is used to create a new array with the morphtargets using the 
 //source vertices and goal vertices where they match in the desired geometry
-const fs = require('fs');
 
-const { vertices } = JSON.parse(fs.readFileSync('./vertices_nose.json'));
+import vertices from './Nose_boss.json' assert { type: "json" };
 
-console.log(vertices);
+//console.log(vertices); // output 'testing'
 
-function moprph_array(source, goal, indices){
-    for (let i = 0; i < source.length; i++) {
-        if(indices.includes(i)) source[i] = goal[i];
+export function morph_array(source, goal, indices){
+
+    console.log(vertices); // output 'testing'
+
+    // for (let i = 0; i < source.length; i++) {
+    //     if(indices.includes(i)) source[i] = goal[i];
         
-    }
-    return source
+    // }
+    // return source
 }
-
-
-
